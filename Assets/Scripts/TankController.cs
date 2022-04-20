80454a4f43972336982affec464f4bab54b4032f
Heemo2000 <75786309+Heemo2000@users.noreply.github.com>
2022-04-20 12:06:41 +0530

    Chapter 7 completed

---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class TankController
+{
+    private TankModel _model;
+
+    private TankView _view;
+
+    public TankController(TankModel model,TankView view)
+    {
+        _model = model;
+        _view = view;
+        GameObject.Instantiate(_view.gameObject);
+        
+        _model.SetTankController(this);        
+    }
+}
--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class TankSpawner : MonoBehaviour
 {
+    [SerializeField]private TankView tankView;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateTank();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void CreateTank()
+    {
+        TankModel tankModel = new TankModel();
+        TankController controller = new TankController(tankModel,tankView);
         
     }
 }
